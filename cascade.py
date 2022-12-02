a6ae9bbc242c70a0288c5b5a6b27eca55be89eb1
dassaniGeet <83092336+dassaniGeet@users.noreply.github.com>
2022-12-02 23:11:55 +0530

    Add files via upload

--- cascade.py
+++ cascade.py
@@ -1,35 +1,43 @@
-from adaboost import * 
-
-
+from adaboost import *
 
 class Cascade:
-    def __init__(self, layers):
-        self.layers = layers
+    def __init__(self, layer):
         self.clfs = []
-
-    def train(self, training):
-        pos, neg = [], []
-        for ex in training:
-            if ex[1] == 1:
-                pos.append(ex)
-            else:
-                neg.append(ex)
-        
-        for feature_num in self.layers:
-            if len(neg) == 0:
-                print("Stopping early. FPR = 0")
-                break
-            clf = vclassifier(feature_num)
-            clf.train(pos+neg, len(pos), len(neg))
-            self.clfs.append(clf)
-            false_positives = []
-            for ex in neg:
-                if self.classify(ex[0]) == 1:
-                    false_positives.append(ex)
-            neg = false_positives
+        self.layers = layer
 
     def classify(self, image):
+        flag=0
         for clf in self.clfs:
-            if clf.classify(image) == 0:
-                return 0
-        return 1
+            if clf.classify(image) != 0:
+                flag=0
+            else:
+                flag=1
+                break
+        if(flag==1):
+            return 0
+        else:
+            return 1
+
+    def train(self, training):
+        positive = []
+        negative=[]
+
+        for i in training:
+            if (i[1]!=1):
+                negative.append(i)
+            else:
+                positive.append(i)
+        
+        for fn in self.layers:
+            if(len(negative)!=0):
+                clf = vclassifier(fn)
+                clf.train(positive+negative, len(positive), len(negative))
+                self.clfs.append(clf)
+                false_positives = []
+                for ex in negative:
+                    if self.classify(ex[0]) == 1:
+                        false_positives.append(ex)
+                negative = false_positives
+            else:
+                print("Stopped Before. False Positive Ratio(FPR) = 0")
+                break
